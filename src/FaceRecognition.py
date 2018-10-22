d65a7ce41707ec045360e2cbc28610a899e3a239
Flavio Lazzarini <flavio@lazzarini.li>
2018-10-22 11:19:27 +0200

    Implemented first face recognition

--- src/FaceRecognition.py
+++ src/FaceRecognition.py
@@ -1,62 +1,66 @@
+import sys
+
 from naoqi import ALBroker, ALModule
 import time
 
-class PepperFaceRecognition(ALModule):
-    # naoqi sessions
-    tts = None
-    memory = None
-    subscriber = None
-    video_service = None
+class FaceRecognition(ALModule):
 
-    # search options
-    scanning = False
-    searched_person = None
-    adding_person = False
-
-    isalreadydetecting = False
-
-    def __init__(self, name, session):
-        ALModule.__init__(self, name)
-        self.session = session
-        self.tts = session.service("ALTextToSpeech")
-        self.face_recon = session.service("ALFaceDetection")
-        self.video_service = session.service("ALVideoDevice")
-        self.tablet = session.service("ALTabletService")
-
+    def __init__(self, robot):
+        """
+        Initialisation of qi framework and event detection.
+        """
+        super(FaceRecognition, self)
+        session = robot.session
+        # Get the service ALMemory.
         self.memory = session.service("ALMemory")
+        # Connect the event callback.
         self.subscriber = self.memory.subscriber("FaceDetected")
-        self.face_recon.subscribe(name)
-        isalreadydetecting = 0
-        self.subscriber.signal.connect(self.faceCallback)
+        self.subscriber.signal.connect(self.on_human_tracked)
+        # Get the services ALTextToSpeech and ALFaceDetection.
+        self.tts = session.service("ALTextToSpeech")
+        self.face_detection = session.service("ALFaceDetection")
+        self.face_detection.subscribe("FaceRecognition")
+        self.got_face = False
 
-    def faceCallback(self, *_args):
-        """Callback method for faceDetected"""
+    def on_human_tracked(self, value):
+        """
+        Callback for event FaceDetected.
+        """
+        if value == []:  # empty value when the face disappears
+            self.got_face = False
+        elif not self.got_face:  # only speak the first time a face appears
+            self.got_face = True
+            print "I saw a face!"
+            self.tts.say("Hello, you!")
+            # First Field = TimeStamp.
+            timeStamp = value[0]
+            print "TimeStamp is: " + str(timeStamp)
 
-        if not self.isalreadydetecting:
-            self.isalreadydetecting = True
-            self.tts.say('hello, im taking a picture')
-            time.sleep(1)
-            self.tts.say("Snap")
-            responsedetect = afa.detectFaceBinary(picture_path[0])
+            # Second Field = array of face_Info's.
+            faceInfoArray = value[1]
+            for j in range(len(faceInfoArray) - 1):
+                faceInfo = faceInfoArray[j]
 
-            if (responsedetect == "fehler keine person erkannt"):
-                self.tts.say('Sorry i could not recognize a face')
-                time.sleep(2)
-                self.isalreadydetecting = False  # TODO change with google
+                # First Field = Shape info.
+                faceShapeInfo = faceInfo[0]
 
-            responseidentify = afa.identifyFace(responsedetect)
+                # Second Field = Extra info (empty for now).
+                faceExtraInfo = faceInfo[1]
 
-            if (responseidentify == "person nicht bekannt"):
-                self.tts.say("I dont know you yet. Do you want to tell me your name?")
-                self.newPerson()
-            else:
-                self.tts.say("I'm looking for your name")
-                personName = afa.getPerson(responseidentify)
-                self.tts.say("hello" + personName);
-                self.tts.say("Have a nice day " + personName)
+                print "Face Infos :  alpha %.3f - beta %.3f" % (faceShapeInfo[1], faceShapeInfo[2])
+                print "Face Infos :  width %.3f - height %.3f" % (faceShapeInfo[3], faceShapeInfo[4])
+                print "Face Extra Infos :" + str(faceExtraInfo)
 
-                time.sleep(2)
-                self.isalreadydetecting = False  # TODO change with google
-
-            time.sleep(5)
-
+    def run(self):
+        """
+        Loop on, wait for events until manual interruption.
+        """
+        print "Starting HumanGreeter"
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            print "Interrupted by user, stopping HumanGreeter"
+            self.face_detection.unsubscribe("HumanGreeter")
+            # stop
+            sys.exit(0)
