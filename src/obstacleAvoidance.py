878f6ed0c2e3c6f4ce38b8609cbc5b59ade1f63c
andrinbuerli <andrin@bluemail.ch>
2018-11-05 12:20:08 +0100

    Revert "Merge remote-tracking branch 'origin/develop' into develop"
    This reverts commit b10ce65e28c99f4302c4ea9a126ce6e4a26b509f, reversing
    changes made to 22a5eebbda39a88716b31696f0b13c4484467580.

--- src/obstacleAvoidance.py
+++ src/obstacleAvoidance.py
@@ -2,26 +2,19 @@ from pynaoqi_mate import Robot
 from configuration import PepperConfiguration
 from naoqi import ALModule
 
-class ObstacleAvoidance():
+class ObstacleAvoidance(ALModule):
 
-
-    def __init__(self, pepper, motion):
-        # super(ObstacleAvoidance, self)
+    def __init__(self, pepper):
         session = pepper.session
-        self.motion = motion
+        self.motion = session.service("ALMotion")
         self.posture = session.service("ALRobotPosture")
         self.setExternalCollisionProtection()
-        self.found = False
-
-    def set_found(self, found):
-        self.found = found
 
     def setExternalCollisionProtection(self):
         self.motion.setExternalCollisionProtectionEnabled("All", True)
 
-    def moveTo(self, distance=1.0, theta=0.1):
-        while not self.found:
-            self.motion.moveTo(distance, 0, theta)
-            if self.posture.getPostureFamily() == "Standing":
-                self.motion.moveTo(-.1, 0, 0)
-                self.motion.moveTo(0, 0, -0.4)
+    def moveTo(self, distance, theta):
+        self.motion.moveTo(distance, 0, theta)
+        if self.posture.getPostureFamily() == "Standing":
+            self.motion.moveTo(-.1, 0, 0)
+            self.motion.moveTo(0, 0, -0.4)
